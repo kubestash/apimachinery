787e9ea99960ede56bb00e1a03fec048c7323818
Emruz Hossain <emruz@appscode.com>
2021-12-20 12:56:25 +0600

    Add comment in BackupStorage API
    Signed-off-by: Emruz Hossain <emruz@appscode.com>

--- apis/storage/v1alpha1/backupstorage_types.go
+++ apis/storage/v1alpha1/backupstorage_types.go
@@ -21,10 +21,12 @@ import (
 	"stash.appscode.dev/kubestash/apis"
 )
 
+// BackupStorage specifies the backend information where the backed up data of different applications will be stored.
+// You can consider BackupStorage as a representation of a bucket in Kubernetes native way.
+// This is a namespaced object. However, you can use the BackupStorage from any namespace
+// as long as it is permitted by the `.spec.usagePolicy` field.
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
-
-// BackupStorage is the Schema for the backupstorages API
 type BackupStorage struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -33,29 +35,74 @@ type BackupStorage struct {
 	Status BackupStorageStatus `json:"status,omitempty"`
 }
 
-// BackupStorageSpec defines the desired state of BackupStorage
+// BackupStorageSpec defines information regarding remote backend, its access credentials, usage policy etc.
 type BackupStorageSpec struct {
-	Storage        Backend           `json:"storage,omitempty"`
-	UsagePolicy    *apis.UsagePolicy `json:"usagePolicy,omitempty"`
-	Default        bool              `json:"default,omitempty"`
-	DeletionPolicy DeletionPolicy    `json:"deletionPolicy,omitempty"`
+	// Storage specifies the remote storage information
+	Storage Backend `json:"storage,omitempty"`
+	// UsagePolicy specifies a policy of how this BackupStorage will be used. For example, you can use `allowedNamespaces`
+	// policy to restrict the usage of this BackupStorage to particular namespaces.
+	// This field is optional. If you don't provide this usagePolicy, then it can be used only from the current namespace.
+	// +optional
+	UsagePolicy *apis.UsagePolicy `json:"usagePolicy,omitempty"`
+
+	// Default specifies whether to use this BackupStorage as default storage for the current namespace
+	// as well as the allowed namespaces.
+	// +optional
+	Default bool `json:"default,omitempty"`
+
+	// DeletionPolicy specifies what to do when you delete a BackupStorage CR.
+	// The valid values are:
+	// "Delete": This will delete the respective Repository and Snapshot CRs from the cluster but keep the backed up data in the remote backend. This is the default behavior.
+	// "WipeOut": This will delete the respective Repository and Snapshot CRs as well as the backed up data from the backend.
+	// +kubebuilder:validation:default=Delete
+	// +optional
+	DeletionPolicy DeletionPolicy `json:"deletionPolicy,omitempty"`
 }
 
 // BackupStorageStatus defines the observed state of BackupStorage
 type BackupStorageStatus struct {
-	Ready        bool               `json:"ready,omitempty"`
-	TotalSize    string             `json:"totalSize,omitempty"`
-	Repositories []RepositoryInfo   `json:"repositories,omitempty"`
-	Conditions   []metav1.Condition `json:"conditions,omitempty"`
+	// Ready specifies whether the BackupStorage is ready to use or not.
+	// +optional
+	Ready bool `json:"ready,omitempty"`
+
+	// TotalSize represent the total backed up data size in this storage.
+	// This is simply the summation of sizes of all Repositories using this BackupStorage.
+	// +optional
+	TotalSize string `json:"totalSize,omitempty"`
+
+	// Repositories holds the information of all Repositories using this BackupStorage
+	// +optional
+	Repositories []RepositoryInfo `json:"repositories,omitempty"`
+
+	// Conditions represents list of conditions regarding this BackupStorage
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
+// RepositoryInfo specifies information regarding a Repository using the BackupStorage
 type RepositoryInfo struct {
-	Name      string  `json:"name,omitempty"`
-	Namespace string  `json:"namespace,omitempty"`
-	Path      string  `json:"path,omitempty"`
-	Size      string  `json:"size,omitempty"`
-	Synced    bool    `json:"synced,omitempty"`
-	Error     *string `json:"error,omitempty"`
+	// Name represent the name of the respective Repository CR
+	Name string `json:"name,omitempty"`
+
+	// Namespace represent the namespace where the Repository CR has been created
+	// +optional
+	Namespace string `json:"namespace,omitempty"`
+
+	// Path represents the directory inside the BackupStorage where this Repository is storing its data
+	// This path is relative to the path of BackupStorage.
+	Path string `json:"path,omitempty"`
+
+	// Size represents the size of the backed up data in this Repository
+	// +optional
+	Size string `json:"size,omitempty"`
+
+	// Synced specifies whether this Repository state has been synced with the cloud state or not
+	// +optional
+	Synced bool `json:"synced,omitempty"`
+
+	// Error specifies the reason in case of Repository sync failure.
+	// +optional
+	Error *string `json:"error,omitempty"`
 }
 
 //+kubebuilder:object:root=true
