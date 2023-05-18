471563d0ee213d6082fc8e17f0019f778df317e0
Md. Ishtiaq Islam <ishtiaq@appscode.com>
2023-05-18 13:55:40 +0600

    Add default value for listMapKey
    Signed-off-by: Md. Ishtiaq Islam <ishtiaq@appscode.com>

--- apis/core/v1alpha1/restoresession_types.go
+++ apis/core/v1alpha1/restoresession_types.go
@@ -220,6 +220,7 @@ const (
 // ComponentRestoreStatus represents the restore status of individual components
 type ComponentRestoreStatus struct {
 	// Name indicate to the name of the component
+	// +kubebuilder:default=""
 	Name string `json:"name,omitempty"`
 
 	// Phase represents the restore phase of the component
