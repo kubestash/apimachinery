126c359043bc2fd7ec4df1a1d136b85ae3bdcea0
Tamal Saha <tamal@appscode.com>
2024-06-04 11:07:41 -0700

    Fork ControllerManagerConfigurationSpec (#114)
    It has been removed from the controller-runtime in 0.18.x release.
    
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/config/v1alpha1/kubestashconfig_types.go
+++ apis/config/v1alpha1/kubestashconfig_types.go
@@ -18,7 +18,6 @@ package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	cfg "sigs.k8s.io/controller-runtime/pkg/config/v1alpha1"
 )
 
 //+kubebuilder:object:root=true
@@ -28,7 +27,7 @@ type KubeStashConfig struct {
 	metav1.TypeMeta `json:",inline"`
 
 	// ControllerManagerConfigurationSpec returns the contfigurations for controllers
-	cfg.ControllerManagerConfigurationSpec `json:",inline"`
+	ControllerManagerConfigurationSpec `json:",inline"`
 
 	// Configuration options related to license
 	License LicenseOptions `json:"license,omitempty"`
