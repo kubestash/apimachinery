c764b5f5a4e44025ecad06610c396714e41307a1
Md. Ishtiaq Islam <ishtiaq@appscode.com>
2024-09-30 12:19:00 +0600

    Replace manifest bool values to bool pointer
    Signed-off-by: Md. Ishtiaq Islam <ishtiaq@appscode.com>

--- apis/core/v1alpha1/restoresession_types.go
+++ apis/core/v1alpha1/restoresession_types.go
@@ -130,7 +130,7 @@ type MSSQLServerManifestOptions struct {
 	// DB specifies whether to restore the DB manifest or not
 	// +kubebuilder:default=true
 	// +optional
-	DB bool `json:"db,omitempty"`
+	DB *bool `json:"db,omitempty"`
 
 	// DBName specifies the new name of the DB yaml after restore
 	// +optional
@@ -139,7 +139,7 @@ type MSSQLServerManifestOptions struct {
 	// AuthSecret specifies whether to restore the AuthSecret manifest or not
 	// +kubebuilder:default=true
 	// +optional
-	AuthSecret bool `json:"authSecret,omitempty"`
+	AuthSecret *bool `json:"authSecret,omitempty"`
 
 	// AuthSecretName specifies new name of the AuthSecret yaml after restore
 	// +optional
@@ -162,7 +162,7 @@ type DruidManifestOptions struct {
 	// DB specifies whether to restore the DB manifest or not
 	// +kubebuilder:default=true
 	// +optional
-	DB bool `json:"db,omitempty"`
+	DB *bool `json:"db,omitempty"`
 
 	// DBName specifies the new name of the DB yaml after restore
 	// +optional
@@ -171,7 +171,7 @@ type DruidManifestOptions struct {
 	// AuthSecret specifies whether to restore the AuthSecret manifest or not
 	// +kubebuilder:default=true
 	// +optional
-	AuthSecret bool `json:"authSecret,omitempty"`
+	AuthSecret *bool `json:"authSecret,omitempty"`
 
 	// AuthSecretName specifies new name of the AuthSecret yaml after restore
 	// +optional
@@ -180,7 +180,7 @@ type DruidManifestOptions struct {
 	// ConfigSecret specifies whether to restore the ConfigSecret manifest or not
 	// +kubebuilder:default=true
 	// +optional
-	ConfigSecret bool `json:"configSecret,omitempty"`
+	ConfigSecret *bool `json:"configSecret,omitempty"`
 
 	// ConfigSecretName specifies new name of the ConfigSecret yaml after restore
 	// +optional
@@ -189,7 +189,7 @@ type DruidManifestOptions struct {
 	// DeepStorageSecret specifies whether to restore the DeepStorageSecret manifest or not
 	// +kubebuilder:default=true
 	// +optional
-	DeepStorageSecret bool `json:"deepStorageSecret,omitempty"`
+	DeepStorageSecret *bool `json:"deepStorageSecret,omitempty"`
 }
 
 type KubeDBManifestOptions struct {
@@ -200,7 +200,7 @@ type KubeDBManifestOptions struct {
 	// DB specifies whether to restore the DB manifest or not
 	// +kubebuilder:default=true
 	// +optional
-	DB bool `json:"db,omitempty"`
+	DB *bool `json:"db,omitempty"`
 
 	// DBName specifies the new name of the DB yaml after restore
 	// +optional
@@ -209,7 +209,7 @@ type KubeDBManifestOptions struct {
 	// AuthSecret specifies whether to restore the AuthSecret manifest or not
 	// +kubebuilder:default=true
 	// +optional
-	AuthSecret bool `json:"authSecret,omitempty"`
+	AuthSecret *bool `json:"authSecret,omitempty"`
 
 	// AuthSecretName specifies new name of the AuthSecret yaml after restore
 	// +optional
@@ -218,7 +218,7 @@ type KubeDBManifestOptions struct {
 	// ConfigSecret specifies whether to restore the ConfigSecret manifest or not
 	// +kubebuilder:default=true
 	// +optional
-	ConfigSecret bool `json:"configSecret,omitempty"`
+	ConfigSecret *bool `json:"configSecret,omitempty"`
 
 	// ConfigSecretName specifies new name of the ConfigSecret yaml after restore
 	// +optional
@@ -227,7 +227,7 @@ type KubeDBManifestOptions struct {
 	// InitScript specifies whether to restore the InitScript manifest or not
 	// +kubebuilder:default=true
 	// +optional
-	InitScript bool `json:"initScript,omitempty"`
+	InitScript *bool `json:"initScript,omitempty"`
 
 	// TLSIssuerRef specifies the name of the IssuerRef used for TLS configurations for both client and server
 	// +optional
