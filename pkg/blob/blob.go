60d1f3e36b65eb77bc901b197b37b30177f73e96
Anisur Rahman <anisur@appscode.com>
2025-01-23 14:36:32 +0600

    Refactor for IRSA
    Signed-off-by: Anisur Rahman <anisur@appscode.com>

--- pkg/blob/blob.go
+++ pkg/blob/blob.go
@@ -60,7 +60,6 @@ const (
 	caCertData                   = "CA_CERT_DATA"
 	awsAccessKeyId               = "AWS_ACCESS_KEY_ID"
 	awsSecretAccessKey           = "AWS_SECRET_ACCESS_KEY"
-	awsRoleArn                   = "AWS_ROLE_ARN"
 )
 
 type Blob struct {
@@ -417,13 +416,13 @@ func closeBucket(ctx context.Context, bucket *blob.Bucket) {
 
 func (b *Blob) getS3Config(ctx context.Context, debug bool) (aws2.Config, error) {
 	var loadOptions []func(*config.LoadOptions) error
-
-	if b.backupStorage.Spec.Storage.S3.Endpoint != "" {
-		config.WithBaseEndpoint(b.backupStorage.Spec.Storage.S3.Endpoint)
-	}
-
-	if b.backupStorage.Spec.Storage.S3.Region != "" {
-		loadOptions = append(loadOptions, config.WithRegion(b.backupStorage.Spec.Storage.S3.Region))
+	if b.backupStorage.Spec.Storage.S3.SecretName != "" {
+		if b.backupStorage.Spec.Storage.S3.Endpoint != "" {
+			config.WithBaseEndpoint(b.backupStorage.Spec.Storage.S3.Endpoint)
+		}
+		if b.backupStorage.Spec.Storage.S3.Region != "" {
+			loadOptions = append(loadOptions, config.WithRegion(b.backupStorage.Spec.Storage.S3.Region))
+		}
 	}
 
 	if debug {
