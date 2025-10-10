f27c9272541f1fd04f79ed3d4f53b4f446067778
Anisur Rahman <anisur@appscode.com>
2025-10-10 17:42:30 +0600

    Introduce WorkerPool Pattern

--- pkg/blob/blob.go
+++ pkg/blob/blob.go
@@ -21,15 +21,17 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
-	aws2 "github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/credentials"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"io"
 	"net/http"
 	"os"
 	"path"
 	"strings"
+
+	aws2 "github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"kubestash.dev/apimachinery/pkg/workerpool"
 
 	"gocloud.dev/blob"
 	_ "gocloud.dev/blob/azureblob"
@@ -39,7 +41,6 @@ import (
 	_ "gocloud.dev/blob/s3blob"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/errors"
 	"k8s.io/klog/v2"
 	"kubestash.dev/apimachinery/apis"
 	storageapi "kubestash.dev/apimachinery/apis/storage/v1alpha1"
@@ -63,11 +64,12 @@ const (
 )
 
 type Blob struct {
-	prefix        string
-	storageURL    string
-	s3Secret      *v1.Secret
-	client        client.Client
-	backupStorage *storageapi.BackupStorage
+	prefix         string
+	storageURL     string
+	maxConnections int64
+	s3Secret       *v1.Secret
+	client         client.Client
+	backupStorage  *storageapi.BackupStorage
 }
 
 func NewBlob(ctx context.Context, c client.Client, bs *storageapi.BackupStorage) (*Blob, error) {
@@ -93,10 +95,11 @@ func s3Blob(ctx context.Context, c client.Client, bs *storageapi.BackupStorage) 
 	}
 
 	return &Blob{
-		client:        c,
-		backupStorage: bs,
-		s3Secret:      secret,
-		prefix:        bs.Spec.Storage.S3.Prefix,
+		client:         c,
+		backupStorage:  bs,
+		s3Secret:       secret,
+		maxConnections: bs.Spec.Storage.S3.MaxConnections,
+		prefix:         bs.Spec.Storage.S3.Prefix,
 	}, err
 }
 
@@ -111,9 +114,10 @@ func gcsBlob(ctx context.Context, c client.Client, bs *storageapi.BackupStorage)
 		}
 	}
 	return &Blob{
-		backupStorage: bs,
-		prefix:        bs.Spec.Storage.GCS.Prefix,
-		storageURL:    fmt.Sprintf("%s%s", gcsPrefix, bs.Spec.Storage.GCS.Bucket),
+		backupStorage:  bs,
+		prefix:         bs.Spec.Storage.GCS.Prefix,
+		maxConnections: bs.Spec.Storage.GCS.MaxConnections,
+		storageURL:     fmt.Sprintf("%s%s", gcsPrefix, bs.Spec.Storage.GCS.Bucket),
 	}, nil
 }
 
@@ -135,16 +139,18 @@ func azureBlob(ctx context.Context, c client.Client, bs *storageapi.BackupStorag
 		}
 	}
 	return &Blob{
-		backupStorage: bs,
-		prefix:        bs.Spec.Storage.Azure.Prefix,
-		storageURL:    fmt.Sprintf("%s%s", azurePrefix, bs.Spec.Storage.Azure.Container),
+		backupStorage:  bs,
+		prefix:         bs.Spec.Storage.Azure.Prefix,
+		maxConnections: bs.Spec.Storage.Azure.MaxConnections,
+		storageURL:     fmt.Sprintf("%s%s", azurePrefix, bs.Spec.Storage.Azure.Container),
 	}, nil
 }
 
 func localBlob(bs *storageapi.BackupStorage) (*Blob, error) {
 	return &Blob{
-		backupStorage: bs,
-		storageURL:    fmt.Sprintf("%s%s?no_tmp_dir=true", localPrefix, bs.Spec.Storage.Local.MountPath),
+		backupStorage:  bs,
+		maxConnections: bs.Spec.Storage.Local.MaxConnections,
+		storageURL:     fmt.Sprintf("%s%s?no_tmp_dir=true", localPrefix, bs.Spec.Storage.Local.MountPath),
 	}, nil
 }
 
@@ -348,8 +354,10 @@ func (b *Blob) deleteDir(ctx context.Context, dir string) error {
 		return err
 	}
 	defer closeBucket(ctx, bucket)
-	var deleteErrs []error
 	iter := bucket.List(nil)
+
+	workerCount := min(b.maxConnections, 10) // Default workers for deleteDir is 10.
+	wp := workerpool.NewWorkerPool(ctx, workerCount)
 	for {
 		obj, err := iter.Next(ctx)
 		if err == io.EOF {
@@ -359,12 +367,18 @@ func (b *Blob) deleteDir(ctx context.Context, dir string) error {
 			return err
 		}
 		filePath := fmt.Sprintf("%s/%s", dir, obj.Key)
-		err = b.Delete(ctx, filePath, false)
-		if err != nil {
-			deleteErrs = append(deleteErrs, err)
-		}
+		wp.Run(func() error {
+			if err := b.Delete(ctx, filePath, false); err != nil {
+				return err
+			}
+			return nil
+		})
 	}
-	return errors.NewAggregate(deleteErrs)
+
+	if err = wp.Wait(); err != nil {
+		return fmt.Errorf("failed to delete directory %s. Err: %w", dir, err)
+	}
+	return nil
 }
 
 func checkIfObjectFile(obj *blob.ListObject) bool {
