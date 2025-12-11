cb49f99325aacee13b1979dd4d25860271d4cbc0
PulokSaha0706 <puloksaha@appscode.com>
2025-12-11 12:14:22 +0600

    Adding listIterator
    Signed-off-by: PulokSaha0706 <puloksaha@appscode.com>

--- pkg/blob/blob.go
+++ pkg/blob/blob.go
@@ -333,6 +333,20 @@ func (b *Blob) List(ctx context.Context, dir string) ([][]byte, error) {
 	return objects, nil
 }
 
+func (b *Blob) ListIterator(ctx context.Context, dir string) (*blob.ListIterator, func(), error) {
+	bucket, err := b.openBucket(ctx, dir)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	cleanup := func() {
+		closeBucket(ctx, bucket)
+	}
+
+	iter := bucket.List(nil) // this is *blob.ListIterator
+	return iter, cleanup, nil
+}
+
 func (b *Blob) Delete(ctx context.Context, filepath string, isDir bool) error {
 	if isDir {
 		return b.deleteDir(ctx, filepath)
